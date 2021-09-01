20bd0bed63c8addf8953c981d4eb0fc80294f6ef
JhairV <jhairvasquez2002@gmail.com>
2021-08-31 22:57:31 -0500

    select dinamico y correccion de formulario

--- app.py
+++ app.py
@@ -13,32 +13,32 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         cedula=request.form['cedula']
-        nombreC=request.form['nombreC']
-        dirC=request.form['dirC']
-        emailC=request.form['emailC']
-        telC=request.form['telC']
         idD=request.form['idD']
-        nomD=request.form['nomD']
-        dirD=request.form['dirD']
-        emailD = request.form['emailD']
-        telD = request.form['telD']
         idP = request.form['idP']
         idC = request.form['idC']
         desP = request.form['desP']
         uniP = request.form['uniP']
         pesoP = request.form['pesoP']
         estadoP = request.form['estadoP']
-        sql='select InsertIntables (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-        values = (cedula, nombreC, dirC, emailC, telC, idD, nomD, dirD, emailD, telD, idP, idC, desP, uniP, pesoP, estadoP)
-        cursor.execute(sql, values)
+        sql="insert into paquete values ({0},'{1}',{2},{3},'{4}','{5}',{6},'{7}')".format(idP, cedula, idC, idD, desP, uniP, pesoP,estadoP)
+        cursor.execute(sql)
         conexion.commit()
         return redirect('/')
     if request.method == 'GET':
         sql = "select id_paquete, descripcion_paquete, estado_paquete, nom_cliente, nom_destinatario, dir_destinatario, nom_conductor, marca_vehiculo, placa_transporte from paquete as p inner join clientes as cl on p.fk_id_cliente= cl.id_cliente inner join conductores as c on p.fk_id_conductor = c.id_conductor inner join destinatario as d on fk_id_destinatario = d.id_destinatario inner join conductores_transportes as ct on ct.id_conductores = c.id_conductor inner join transportes as t on t.id_transporte = ct.id_transportes where p.estado_paquete = 'Registrado'"
         cursor.execute(sql)
         paquetes = cursor.fetchall()
+        sqlClientes= "select * from clientes"
+        cursor.execute(sqlClientes)
+        clientes = cursor.fetchall()
+        sqlDestinatarios = "select * from destinatario"
+        cursor.execute(sqlDestinatarios)
+        destinatarios=cursor.fetchall()
+        sqlConductores = "select * from conductores"
+        cursor.execute(sqlConductores)
+        conductores = cursor.fetchall()
         conexion.commit()
-        return render_template('create.html', paquetes = paquetes)
+        return render_template('create.html', paquetes = paquetes, clientes = clientes, destinatarios = destinatarios, conductores =conductores)
     return render_template('create.html')
 
 @app.route('/conductores')
@@ -152,3 +152,33 @@ def destinatarios():
     destinatarios = cursor.fetchall()
     conexion.commit()
     return render_template('destinatarios.html', destinatarios=destinatarios)
+
+@app.route('/editDestinatario/<id>', methods=['GET', 'POST'])
+def editDestinatario(id):
+    sql = "select * from destinatario where id_destinatario='{0}'".format(id)
+    cursor.execute(sql)
+    destinatarios = cursor.fetchall()
+    conexion.commit()
+    return render_template('editDestinatario.html', destinatarios = destinatarios)
+
+@app.route('/updateDestinatario', methods=['POST'])
+def updateDestinatario():
+    _idD= request.form['idD']
+    _nombreD = request.form['nombreD']
+    _dirD = request.form['dirD']
+    _emailD = request.form['emailD']
+    _telD = request.form['telD']
+    sql = "update destinatario set nom_destinatario='{1}', dir_destinatario='{2}', email_destinatario='{3}', telefono_destinatario='{4}' where id_destinatario='{0}'".format(_idD, _nombreD, _dirD, _emailD, _telD)
+    cursor.execute(sql)
+    conexion.commit()
+    return redirect('/destinatarios')
+
+
+@app.route('/entregados')
+
+def entregados():
+    sql = "select  id_paquete, descripcion_paquete, estado_paquete, nom_cliente, nom_destinatario, dir_destinatario, nom_conductor, marca_vehiculo, placa_transporte from paquete as p inner join clientes as cl on fk_id_cliente = id_cliente inner join conductores as c on p.fk_id_conductor = c.id_conductor inner join destinatario as d on fk_id_destinatario = d.id_destinatario inner join conductores_transportes as ct on ct.id_conductores = c.id_conductor inner join transportes as t on t.id_transporte = ct.id_transportes where p.estado_paquete = 'Entregado'"
+    cursor.execute(sql)
+    paquetes = cursor.fetchall()
+    conexion.commit()
+    return render_template('entregados.html', paquetes = paquetes)
